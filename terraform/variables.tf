7d49d79cb9ea9ea94f284ace7ca45818fa0d76c5
unknown <reachfinbarr@gmailm>
2026-05-21 19:50:19 +0100

    Change default location from eastus to canadacentral

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "eastus"
+  default     = "canadacentral"
 }
 
 variable "acr_name" {
